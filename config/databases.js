078568c6290132c4dc98c0f7b05d86d0c841007f
Deepakgantayat <deepakgantayat29@gmail.com>
2023-07-11 18:22:50 +0530

    connection added

---
+++ config/databases.js
@@ -0,0 +1,17 @@
+import { connect } from 'mongoose';
+
+let conn;
+export const connectDB = async () => {
+  try {
+    conn = await connect("mongodb+srv://deepakgantayat29:EwMydJq1Bpaj5Mp1@cluster0.hsiamn8.mongodb.net/?retryWrites=true&w=majority");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const disconnectDB = async () => {
+  if (conn) {
+    await conn.disconnect();
+  }
+};
