fae3b2466013c993e22c2c80d367d3626b0bc1f0
Deepakgantayat <deepakgantayat29@gmail.com>
2023-07-12 07:59:25 +0530

    trying to fix production error

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-import express, { json } from 'express'
+import express from 'express'
 import cors from 'cors'
 // import {connectDB} from './config/databases.js'
 import router from './config/routes.js'
 
 const app = express()
 const port = 3002
-// app.use(json())
-// app.use(cors())
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req,res) => {
-    res.send('Welcome to the page')
+app.get('/',(req,res)=>{
+    res.send('welcome to the app')
 })
 
-// app.use('/',router)
-// app.get('/users/data', (req,res) => {
-//   res.send('Welcome to the page')
-// })
+app.use('/',router)
 
-// connectDB().then(
-//     async () => {
-      app.listen(port, () => {
-        console.log('listening to the port,',port)
-    })
-  //   },
-  //   () => {
-  //     console.log('Cannot start app until database connection is initialized. This will become async later, but for the time, bad luck :(');
-  //   }
-  // );
+app.listen(port,()=>{
+    console.log('listening on port ',port)
+})
+
